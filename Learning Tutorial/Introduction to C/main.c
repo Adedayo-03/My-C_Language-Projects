2c2eeb4d5495f4c6ba42fb1f120f1866b9c028ee
Adedayo-03 <princeadedayo03@gmail.com>
2022-10-08 04:00:23 +0100

    print and comments

---
+++ Learning Tutorial/Introduction to C/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Call me D'Prince \n");
+    printf ("I am Software engineer \n");
+    printf ("I write code in the following languages:\n C,\n C++,\n C#,\n and Java");
+
+    // Comments : They re used to explain codes and make it more readable
+    // It can a so be used to prevent execution (especially when testing alternate codes)
+    //This is a single line comment
+    /* This is a multi line comment
+        It is used to comment a block of codes
+    */
+
+    return 0;
+}
